d102e169fee69f3f4c46d20aa1e7bd720d2d4b91
gustavvopenna <gustavo.pema@gmail.com>
2019-06-25 21:25:15 -0500

    Setup project

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+// Main function is the staring point for all flutter apps
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    ),
+  );
+}
